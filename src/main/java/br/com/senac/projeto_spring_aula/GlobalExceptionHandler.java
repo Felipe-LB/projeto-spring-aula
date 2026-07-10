3cdef1d3a27d8831be3e6e102a515b3e0fef8cb7
Felipe Lopes Batista <felipelopesb02@gmail.com>
2026-07-09 21:44:13 -0300

    adicionado métodos

---
+++ src/main/java/br/com/senac/projeto_spring_aula/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.com.senac.projeto_spring_aula;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<?> handlevalidation(MethodArgumentNotValidException ex) {
+
+            String mensagem = ex.getBindingResult()
+                    .getFieldErrors()
+                    .get(0)
+                    .getDefaultMessage();
+
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("erro", mensagem));
+        }
+}
